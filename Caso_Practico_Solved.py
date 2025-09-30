d437a2f7932fe743f453976c675129a4ce7ecce2
Omar Reyes L. <relo2210@gmail.com>
2025-09-29 23:06:55 -0600

    Reconocimiento de Ganador agregado

--- Caso_Practico_Solved.py
+++ Caso_Practico_Solved.py
@@ -1,5 +1,6 @@
 tablero_header = [("0", "1", "2", "3", "4", "5", "6")]
-
+jugador=1
+finalizar=0
 
 def table_starter(tablero_header):
     tablero = tablero_header
@@ -13,7 +14,49 @@ def mostrar_tablero(tablero):
         print(fila)
         if fila is tablero[0]: print("===================================")
 
-def turno(jugador, tablero):
+def columnas_calculador(tablero):
+    columna_contador_x = [0, 0, 0, 0, 0, 0, 0]
+    columna_contador_0 = [0, 0, 0, 0, 0, 0, 0]
+    fin = 0
+    i = 6
+    j = 0
+    #Recorrido de las filas de la matríz
+    for fila in tablero[:0:-1]:
+        #Recorrido por los elementos de la fila para añadir al contador
+        for columna in fila[:][:]:
+            #Condiciones para reiniciar contador cuando ya no hay igualdad de columnas por fila
+            if i == 6: pass
+            else:
+                if columna_anterior != columna:
+                    if columna_anterior == "x":
+                        columna_contador_x[j] = 0
+                    elif columna_anterior == "0":
+                        columna_contador_0[j] = 0
+
+            #Aumentar contador
+            if columna == "x":
+                columna_contador_x[j] +=1
+            elif columna == "0":
+                columna_contador_0[j] +=1
+            j+=1
+            columna_anterior = columna
+        j= 0
+        i-=1
+    #Reconocer y Guardar al ganador del juego
+    if 4 in columna_contador_x or columna_contador_0:
+        if 4 in columna_contador_x:
+            fin = 1
+            ganador = "El ganador es el Jugador 1"
+            print(ganador)
+        elif 4 in columna_contador_0:
+            fin = 1
+            ganador = "El ganador es el Jugador 2"
+            print(ganador)
+    else:
+        fin = 0
+    return fin
+
+def turno(jugador, tablero, ):
     # Selección de ficha
     if jugador == 1: jugada = "x"
     else: jugada = "0"
@@ -49,14 +92,10 @@ def turno(jugador, tablero):
     i = 6 #Contador de filas
     finalizar = False #Detecta cuando se encontró una localización valida para la ficha
     for fila in tablero[:0:-1]:
-        print("Fila actual:", i)
-        print("Contenido de la fila: ", fila)
         for columna in fila[:][fila_sel]:
-            print("Columna: ", fila_sel)
-            print("Contenido de la columna: ", columna)
             #Comprobación de valor
             if "." in columna:
-                print("CAMBIO!")
+                print("JUGADA REALIZADA!")
                 finalizar = True
                 tablero[i][fila_sel] = jugada
                 break
@@ -67,72 +106,18 @@ def turno(jugador, tablero):
             break
         i -= 1
 
-
-
-
-    ## ====================== <-
-
-
-    ## -- Meter la función que calcule si ya gano alguien y guardarlo en winner
-
-    ## -- Agregar un if winner: break
-
     #Cambiar de jugador automaticamente para el proximo turno
     if jugador == 1: jugador = 0
     else: jugador = 1
 
     return tablero, jugador
 
-def columnas_calculador(tablero):
-    columna_contador_x = [0, 0, 0, 0, 0, 0, 0]
-    columna_contador_0 = [0, 0, 0, 0, 0, 0, 0]
-    i = 6
-    j = 0
-    #Recorrido de las filas de la matríz
-    for fila in tablero[:0:-1]:
-        print(fila)
-        #Recorrido por los elementos de la fila para añadir al contador
-        for columna in fila[:][:]:
-            #Condiciones para reiniciar contador cuando ya no hay igualdad de columnas por fila
-            if i == 6: pass
-            else:
-                if columna_anterior != columna:
-                    print("CAMBIO!")
-                    if columna_anterior == "x":
-                        print("EN X")
-                        columna_contador_x[j] = 0
-                    elif columna_anterior == "0":
-                        print("EN 0")
-                        columna_contador_0[j] = 0
-
-            #Aumentar contador
-            if columna == "x":
-                columna_contador_x[j] +=1
-            elif columna == "0":
-                columna_contador_0[j] +=1
-            j+=1
-            columna_anterior = columna
-
-        j= 0
-        i-=1
-        print(columna_contador_x, columna_contador_0)
-
-        #Reconocer y Guardar al ganador del juego
-        if 4 in columna_contador_x or columna_contador_0:
-            if 4 in columna_contador_x:
-                print("El ganador es el Jugador 1")
-                winner = "El ganador es el Jugador 1"
-            elif 4 in columna_contador_0:
-                print("El Ganador es el Jugador 2")
-                winner = "El ganador es el Jugador 2"
-            else: continue
-    return
-
-jugador=1
+#Simulación del juego
 for i in range(20):
-    break
+    print("Siguiente Jugador: ")
+    if finalizar == 1:
+        break
     movimiento, jugador= turno(jugador, tablero)
-    print("Siguiente Jugador")
+    finalizar = columnas_calculador(movimiento)
 
-tablero = [("0", "1", "2", "3", "4", "5", "6"),[".", ".", ".", ".", ".", ".", "x",], [".", ".", ".", ".", ".", ".", "x",], [".", "0", "0", "0", "0", "x", "x",], [".", "x", "0", "x", "0", "x", "x",] ]
-columnas_calculador(tablero)
+
