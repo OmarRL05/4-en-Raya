765a86ed7f4bdb968e22042735aa2e039f454961
Omar Reyes L. <relo2210@gmail.com>
2025-10-02 09:31:24 -0600

    feat: Se creó la función Optimizada para mostrar el tablero
    1. Se cambió ya que en la primera versión, inicialmente se imprimian directamente las filas del arreglo.
    
    2. Con este cambio se añade una interfaz más limpia en terminal

--- Caso_Practico_Optimized.py
+++ Caso_Practico_Optimized.py
@@ -3,3 +3,16 @@ def crear_tablero(filas, columnas):
     for f in range(filas):
         tablero[f] = ['.']*columnas
     return tablero
+
+def mostrar_tablero(tablero):
+    for num in range(len(tablero[0])):
+        """ Accede a la primera lista
+            [".", ".", ".", "." ... n]
+            len(tablero[0] toma la cantidad
+            "n" de elementos en la lista.
+        """
+        print(num, end='  ')
+    for fila in tablero:
+        print("")
+        for objeto in fila:
+            print(objeto, "|", end=' ')
